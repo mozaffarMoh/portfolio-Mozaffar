5a42745cf146685a2f4d9e8f530a0e6cf94c2f27
Mozaffar <FERAWWWESS@gmail.com>
2024-03-31 00:23:18 +0300

    correct about

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -7,14 +7,14 @@ const About = () => {
       <div className="about-content">
         <h4>Who is Mozaffar ?</h4>
         <p>
-          My name is Mozaffar Mohammad and i have 2 years experience working
-          with React framework, i do alot of real projects, you can see it below
-          in works section .
+          My name is Mozaffar Mohammad, and I have one year of experience
+          working With the React framework, I do a lot of real projects; you can
+          see them below. in works section.
         </p>
         <h4>Personal life</h4>
         <p>
-          I was born on August 1, 1996. I live in Syria in Homs Governorate. My
-          hobbies are playing the violin and playing football.
+          I was born on January 8, 1996. I live in Syria, in Homs Governorate.
+          My My hobbies are playing the violin and playing football.
         </p>
       </div>
     </div>
